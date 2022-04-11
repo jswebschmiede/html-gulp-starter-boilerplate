8a5d35b38edc040ca01df36a334032d5e9285712
jswebschmiede <joergschoeneburg@freenet.de>
2022-04-11 22:58:19 +0200

    browsersync added

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -14,8 +14,8 @@ import cssnano from "cssnano";
 import browserSync from "browser-sync";
 
 // File path variables etc.
+const dev_url = "yourlocal.dev";
 const sass = gulpSass(dartSass);
-const cbString = new Date().getTime();
 const files = {
   scssPath: {
     src: "src/scss/**/*.scss",
@@ -25,6 +25,29 @@ const files = {
     src: "src/js/**/*.js",
     dest: "dist/js",
   },
+};
+
+// Browsersync to spin up a local server
+const browserSyncServe = (cb) => {
+  // initializes browsersync server
+  browserSync.init({
+    server: {
+      baseDir: "./src",
+      // proxy: dev_url,
+    },
+    notify: {
+      styles: {
+        top: "auto",
+        bottom: "0",
+      },
+    },
+  });
+  cb();
+};
+const browserSyncReload = (cb) => {
+  // reloads browsersync server
+  browserSync.reload();
+  cb();
 };
 
 // Sass Task
@@ -54,18 +77,39 @@ const jsTask = () => {
 
 // Cashbusting Task
 const cashbustTask = () => {
+  const cbString = new Date().getTime();
   return src(["src/index.html"])
     .pipe(replace(/cb=\d+/g, "cb=" + cbString))
     .pipe(dest("dist"));
 };
 
-// Webserver Task
+// Browsersync Watch task
+// Watch HTML file for change and reload browsersync server
+// watch SCSS and JS files for changes, run scss and js tasks simultaneously and update browsersync
+const bsWatchTask = () => {
+  watch("src/index.html", browserSyncReload);
+  watch(
+    [files.scssPath.src, files.jsPath.src],
+    { interval: 1000, usePolling: true }, //Makes docker work
+    series(parallel(scssTask, jsTask), cashbustTask, browserSyncReload)
+  );
+};
+
+// Webserver Task (default)
 const webserverTask = () => {
   return src("dist").pipe(
     server({
       livereload: true,
       open: true,
       port: 3030,
+      filter: function (fileName) {
+        // exclude all source maps from livereload
+        if (fileName.match(/.map$/)) {
+          return false;
+        } else {
+          return true;
+        }
+      },
     })
   );
 };
@@ -81,4 +125,11 @@ exports.default = series(
   cashbustTask,
   webserverTask,
   watchTask
+);
+
+exports.bs = series(
+  parallel(scssTask, jsTask),
+  cashbustTask,
+  browserSyncServe,
+  bsWatchTask
 );
